a04df170ce0a0efea84a688f3cb57c82f4616c25
Ladislav Lipka <lipka28@gmail.com>
2022-11-28 20:41:23 +0100

    Nothing to see here

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,60 @@
-extern crate sdl2; 
-
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::rect::{Rect, Point};
+use sdl2::render::{WindowCanvas, Texture};
+use sdl2::image::{self, InitFlag, LoadTexture};
 use std::time::Duration;
- 
-pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
- 
-    let window = video_subsystem.window("rust-sdl2-demo", 640, 480)
+
+fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
+    canvas.set_draw_color(color);
+    canvas.clear();
+    canvas.copy(texture, Rect::new(0, 0, 26, 36), Rect::new(0, 0, 26, 36))?;
+    canvas.present();
+
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    // Leading "_" tells Rust that this is an unused variable that we don't care about. It has to
+    // stay unused because if we don't have any variable at all then Rust will treat it as a
+    // temporary value and drop it right away!
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
+
+    let window = video_subsystem.window("rust-sdl2-test", 800, 600)
         .position_centered()
         .build()
-        .unwrap();
- 
-    let mut canvas = window.into_canvas().build().unwrap();
- 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
+        .expect("could not initialize video subsystem");
+
+    let mut canvas = window.into_canvas().build()
+        .expect("could not make a canvas");
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
+
+    let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+        // Handle events
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
+                    break 'running;
                 },
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
 
-        canvas.present();
+        // Update
+        i = (i + 1) % 255;
+
+        // Render
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture)?;
+
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
+
+    Ok(())
 }
